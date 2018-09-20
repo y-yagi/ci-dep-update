9cc51d9b2eb3b9d59ff364a45f3def17761f1457
yuuji.yaginuma <yuuji.yaginuma@gmail.com>
2018-09-20 09:15:10 +0900

    create new branch after commit

--- dep_updater.go
+++ dep_updater.go
@@ -86,11 +86,11 @@ func (updater *DepUpdater) isNeedUpdate() (bool, error) {
 func (updater *DepUpdater) createBranchAndCommit(username, useremail, token, repo, branch string) {
 	remote := "https://" + token + "@github.com/" + repo
 	exec.Command("git", "remote", "add", "github-url-with-token", remote).Run()
-	exec.Command("git", "checkout", "-b", branch).Run()
 	exec.Command("git", "config", "user.name", username).Run()
 	exec.Command("git", "config", "user.email", useremail).Run()
 	exec.Command("git", "add", "Gopkg.lock").Run()
 	exec.Command("git", "commit", "-m", "Run 'dep ensure -update'").Run()
+	exec.Command("git", "branch", "-M", branch).Run()
 	exec.Command("git", "push", "-q", "github-url-with-token", branch).Run()
 }
 
